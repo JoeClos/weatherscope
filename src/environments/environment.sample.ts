6eafd304cc7580633a9a18a4a56d493f102d23aa
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2025-04-25 20:02:15 +0300

    chore: add environment.sample.ts

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  openWeatherApiKey: 'YOUR_API_KEY',
+  openWeatherGeoUrl: 'https://api.openweathermap.org/geo/1.0/direct',
+  openWeatherBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  openWeatherReverseGeoUrl: 'https://api.openweathermap.org/geo/1.0/reverse',
+};
